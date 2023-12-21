bf898340882a80b1561419cf265d39ac61b9c97b
Rehan Azeem <92761969+rehanazeem@users.noreply.github.com>
2023-12-21 17:42:57 +0500

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from "./Footer.jsx";
+import Hero from "./Hero.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import Services from "./Services.jsx";
+import SpecialOffers from "./SpecialOffers.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    SuperQuality,
+    Subscribe
+}
